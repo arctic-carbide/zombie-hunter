da8624c3401f624266fe84be5a835fb9ec8ff76f
Ian Simon <isimon@umich.edu>
2020-04-10 01:29:38 +0000

    gun shoots no sounds

---
+++ Assets/Script/BulletBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private Transform barrelTip;
+
+    [SerializeField]
+    private GameObject bullet;
+
+    private Vector2 direction;
+    private float lookAngle;
+    // Start is called before the first frame update
+    void Update()
+    {
+        direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        lookAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, lookAngle - 90f);
+        if(Input.GetMouseButtonDown(0)){
+            FireBullet();
+        }
+        
+    }
+
+    private void FireBullet(){
+        GameObject firedBullet = Instantiate(bullet, barrelTip.position, barrelTip.rotation);
+        firedBullet.GetComponent<Rigidbody2D>().velocity = barrelTip.up * 10f;
+        Destroy(firedBullet, 0.5f);
+    }
+}
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -18,12 +18,6 @@ public class PlayerMovement : MonoBehaviour
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position) * Quaternion.Euler(0, 0, 90);
         Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        rb.MovePosition (rb.position + movement_vector * Time.deltaTime * moveSpeed);
-        // if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < 0.5f){
-        //     transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f), Space.World);
-        // }   
-        // if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < 0.5f){
-        //     transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f), Space.World);
-        // }               
+        rb.MovePosition (rb.position + movement_vector * Time.deltaTime * moveSpeed);    
     }
 }
