3b78aba768fd4717ebbcf4aee02a80ac7ae2f337
Mitchell Myers <myersmk@umich.edu>
2020-04-10 05:41:50 +0000

    trying to make a script to play multiple sound clips on the player

---
+++ Assets/Script/PlayerAudio.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    public AudioClip[] hurtSFX = new AudioClip[1];
+    public AudioClip[] deathSFX = new AudioClip[1];
+    
+    private AudioSource source;
+    private System.Random rng = new System.Random();
+    private const string damageTag = "Damage Source";
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) { 
+
+        switch (collision.collider.tag)
+        {
+
+            case damageTag: PlaySound(hurtSFX);
+                break;
+
+        }
+
+    }
+
+    private void PlaySound(params AudioClip[] clips)
+    {
+        Debug.Assert(clips != null);
+
+        if (clips.Length > 0)
+        {
+            source.clip = clips[rng.Next(0, clips.Length) % clips.Length];
+            source.Play();
+        }
+    }
+}
