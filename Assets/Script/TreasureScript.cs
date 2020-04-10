cf17e48ac752d1e80100596aadf746e30a38d66b
Ian Simon <isimon@umich.edu>
2020-04-10 02:18:42 +0000

    grabbing treasure kinda works but there's more to the story

---
+++ Assets/Script/TreasureScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureScript : MonoBehaviour
+{
+    public GameObject treasure;
+    public bool collectedTreasure = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        treasure =  GameObject.Find("Grid/treasure");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("anything", treasure);
+        //if (other.gameObject.tag == "Player") 
+        //{
+            collectedTreasure = true;
+            Destroy (treasure.gameObject);
+        //}
+    }
+}
