93cd40fa00932a799ae882794d29bfec69433133
Mitchell Myers <myersmk@umich.edu>
2020-04-13 07:15:36 +0000

    enemies spawn and drop treasure that you can pick up

---
+++ Assets/Script/SpawnEnemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public GameObject enemy;
+
+    private Transform[] spawnPoints;
+    private int cooldown = 2;
+    private bool locked = false;
+    private int num = 0;
+
+
+    void Start()
+    {
+
+        spawnPoints = GetComponentsInChildren<Transform>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!locked)
+        {
+
+            StartCoroutine(Spawn(spawnPoints[num % spawnPoints.Length]));
+            num++;
+        }
+    }
+
+    private IEnumerator Spawn(Transform spawnPoint)
+    {
+        locked = true;
+        
+        Instantiate(enemy, spawnPoint.transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(cooldown);
+
+        locked = false;
+    }
+}
--- Assets/Script/TreasureScript.cs
+++ Assets/Script/TreasureScript.cs
@@ -5,27 +5,21 @@ using UnityEngine;
 public class TreasureScript : MonoBehaviour
 {
     public GameObject treasure;
-    public bool collectedTreasure = false;
+    //public bool collectedTreasure = false;
     // Start is called before the first frame update
     void Start()
     {
         treasure =  GameObject.Find("Grid/treasure");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("anything", treasure);
+        //Debug.Log("anything", treasure);
         if (other.gameObject.tag == "Player") 
         {
-            collectedTreasure = true;
-            Destroy (treasure.gameObject);
+            //collectedTreasure = true;
 
+            Destroy(gameObject);
             ScoringSystem.Increase(1);
         }
     }
